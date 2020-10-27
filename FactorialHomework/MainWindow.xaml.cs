ba786e4c6857c1c47dc6553fe3192be640f3afee
AlexanderLukashuk <upiter465@gmail.com>
2020-10-27 21:08:48 +0600

    Создано оконное приложение высчитывающее факториал

---
+++ FactorialHomework/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace FactorialHomework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateFactorial(object sender, RoutedEventArgs e)
+        {
+            int number = 0;
+            long factorial = 1;
+
+            if (int.TryParse(numberTextBox.Text, out number))
+            {
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+
+                    if (number == 0)
+                    {
+                        factorial = 1;
+                    }
+                    else
+                    {
+                        for (int i = 1; i <= number; i++)
+                        {
+                            factorial *= i;
+                        }
+                    }
+
+                    MessageBox.Show($"Факториал числа {number} = {factorial}");
+                    MessageBox.Show("Вычисление завершено");
+                });
+
+            }
+            else
+            {
+                MessageBox.Show("Ошибка! Повторите ввод");
+            }
+
+
+        }
+    }
+}
